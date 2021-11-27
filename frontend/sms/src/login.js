6171e541fc4d703973ce311e46211a6cb6a9d984
Robbie Hammond <robbiehammond3@gmail.com>
2021-11-27 09:49:40 -0500

    more react stuff

---
+++ frontend/sms/src/login.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
+
+class LoginPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      error: '',
+    };
+
+    this.handlePassChange = this.handlePassChange.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.dismissError = this.dismissError.bind(this);
+  }
+
+  dismissError() {
+    this.setState({ error: '' });
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    if (!this.state.username) {
+      return this.setState({ error: 'Username is required' });
+    }
+
+    if (!this.state.password) {
+      return this.setState({ error: 'Password is required' });
+    }
+
+    return this.setState({ error: '' });
+  }
+
+  handleUserChange(evt) {
+    this.setState({
+      username: evt.target.value,
+    });
+  };
+
+  handlePassChange(evt) {
+    this.setState({
+      password: evt.target.value,
+    });
+  }
+
+  render() {
+    // NOTE: I use data-attributes for easier E2E testing
+    // but you don't need to target those (any css-selector will work)
+
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          {
+            this.state.error &&
+            <h3 data-test="error" onClick={this.dismissError}>
+              <button onClick={this.dismissError}>✖</button>
+              {this.state.error}
+            </h3>
+          }
+          <TextField type="text" label="username" variant="filled" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
+
+          <TextField type="password" label="password" variant="filled" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
+
+          <Button variant="contained" type="submit" value="Log In" data-test="submit" >Log In</Button>
+        </form>
+
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
