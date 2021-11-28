05ffe1a8ebd6e9ee6fb2fcf6c34583e262ca9d33
Robbie Hammond <robbiehammond3@gmail.com>
2021-11-27 22:30:38 -0500

    function stubs for backend

--- backend/test.py
+++ backend/test.py
@@ -2,28 +2,61 @@ import mysql.connector
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+mycursor = None
 #start backend flask thing
 app = Flask(__name__)
 CORS(app)
 
-#if we try to connect to http://18.219.202.94:5000/test, execute this function
-@app.route("/test")
-def test():
-    #connect to DB as root locally on this PC
-    mydb = mysql.connector.connect(
-        user="root",
-        password="CSDS341",
-        database="test"
-    )
+#Log into DBMS with account. If true, login successful and HTML can change
+#If can't log in, indicate with false
+@app.route("/login")
+def login():
+    global mycursor
+    username = request.form['username']
+    passwrd = request.form['password']
+    try:
+        mydb = mysql.connector.connect(
+            user=username,
+            password=passwrd,
+            database="test" #change this
+        )
+    except Exception:
+        return False
     mycursor = mydb.cursor()
+    return True
 
-    #get all data from it 
-    mycursor.execute("SELECT * FROM relation")
-    l = []
-    for x in mycursor:
-        l.append(x)
-    return str(l)
+#get all subscriptions
+@app.route("/getAll")
+def getAll():
+    pass
 
-#indefinitely run app
+#update subscription by the subscriptions ID
+#params not at thing in Flask, so they'll just be pulled off of the request
+@app.route("/update")
+def update():
+    pass
+
+#Get all subscriptions where some properties are true
+#Certainly gonna be the most complex function. 
+@app.route("/getWhere")
+def getWhere():
+    #Incoming JSON message in form like: 
+    # {
+    #   Platform: Any
+    #   Payment Method: {card number=1234 5352 5345 6929}
+    #   Invoice: {start_date=11-27-2021 or after, amount due=$10 or greater}
+    #   etc etc etc
+    # }
+    pass
+
+
+#Create a new subscription based on properties specified 
+@app.route("/create")
+def create():
+    pass
+
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
