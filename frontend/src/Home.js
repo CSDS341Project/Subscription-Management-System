7bfaa25ba85839fad8fb2b5267a95787adf80a28
Robbie Hammond <robbiehammond3@gmail.com>
2021-12-01 16:28:52 -0500

    some filtering options

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -4,6 +4,9 @@ import { ThemeProvider } from "@mui/styles";
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 import Box from "@mui/material/Box"
 import Button from '@mui/material/Button';
+import FormGroup from '@mui/material/FormGroup'
+import FormLabel from '@mui/material/FormLabel'
+import TextField from '@mui/material/TextField'
 import Stack from '@mui/material/Stack';
 import ws from "./socketConfig"
 import ModalPopup from './popup'
@@ -18,21 +21,22 @@ class HomePage extends Component {
       this.state = {
         showPopup: false,
        subscriptions: [
-         { id: 1, name: 'test_subscription' }
+         { id: 1,
+           platform: 'test_subscription', 
+           start_date: 0, billing_freq: 0, amount: 0,  }
        ],
-       columns: ['name'],
-       info: "Click on a subscription for more data",
+       info: "Click on platform for more data",
        selectedRow: "N/A"
       };
     }
 
     renderData() {
       return this.state.subscriptions.map((item, index) => {
-        const { id, name } = item;
+        const { id, platform } = item;
         return (
           <tr key={id}>
             <td>{id}</td>
-            <td>{name}</td>
+            <td>{platform}</td>
           </tr>
         )
       })
@@ -59,7 +63,7 @@ class HomePage extends Component {
         let subs = message['subscriptions'];
         subs.forEach(function(sub, i) {
           this.setState({
-            subscriptions: [...this.state.subscriptions, {id: i, name: sub}]
+            subscriptions: [...this.state.subscriptions, {id: i, platform: sub}]
           })
         }.bind(this));
       }.bind(this));
@@ -69,7 +73,7 @@ class HomePage extends Component {
       const onRowSelectionChange = (cur, all, rows) => {
         const res = all.map(item => { return this.state.subscriptions.at(item.index) });
         const selected = res.map(item => {
-          return item.name
+          return item.platform
         });
         this.setState({
           selectedRow: selected
@@ -87,13 +91,96 @@ class HomePage extends Component {
       //setup n stuff
       let theme = createTheme()
       theme = responsiveFontSizes(theme);
+
+      const columns = [{
+        name: 'id',
+        options: {
+        filterType: 'custom',
+        customFilterListOptions: {
+          render: v => {
+            if (v[0] && v[1] && this.state.ageFilterChecked) {
+              return [`Min ID: ${v[0]}`, `Max ID: ${v[1]}`];
+            } else if (v[0] && v[1] && !this.state.ageFilterChecked) {
+              return `Min ID: ${v[0]}, Max ID: ${v[1]}`;
+            } else if (v[0]) {
+              return `Min ID: ${v[0]}`;
+            } else if (v[1]) {
+              return `Max ID: ${v[1]}`;
+            }
+            return [];
+          },
+          update: (filterList, filterPos, index) => {
+            console.log('customFilterListOnDelete: ', filterList, filterPos, index);
+
+            if (filterPos === 0) {
+              filterList[index].splice(filterPos, 1, '');
+            } else if (filterPos === 1) {
+              filterList[index].splice(filterPos, 1);
+            } else if (filterPos === -1) {
+              filterList[index] = [];
+            }
+
+            return filterList;
+          },
+        },
+        filterOptions: {
+          names: [],
+          logic(id, filters) {
+            //fix
+            if (filters[0] && filters[1]) {
+              return id < filters[0] || id > filters[1];
+            } else if (filters[0]) {
+              return id < filters[0];
+            } else if (filters[1]) {
+              return id > filters[1];
+            }
+            return false;
+          },
+          display: (filterList, onChange, index, column) => (
+            <div>
+              <FormLabel>ID Number</FormLabel>
+              <FormGroup row>
+                <TextField
+                  label='min'
+                  value={filterList[index][0] || ''}
+                  onChange={event => {
+                    filterList[index][0] = event.target.value;
+                    onChange(filterList[index], index, column);
+                  }}
+                  style={{ width: '45%', marginRight: '5%' }}
+                />
+                <TextField
+                  label='max'
+                  value={filterList[index][1] || ''}
+                  onChange={event => {
+                    filterList[index][1] = event.target.value;
+                    onChange(filterList[index], index, column);
+                  }}
+                  style={{ width: '45%' }}
+                />
+              </FormGroup>
+            </div>
+          ),
+        },
+        print: false
+        }
+      },
+      {
+        name: 'platform',
+        filter: true,
+        filterType: 'textField'
+      }
+    ]
+
+
       const options = {
         selectableRows: 'single',
         selectableRowsHideCheckboxes: true,
         expandableRowsOnClick: true,
         selectableRowsOnClick: true,
+        selectToolbarPlacement: 'none',
+        onRowsDelete: function() { return false },
         onRowSelectionChange,
-
       }
       return (
         <><div>
@@ -105,7 +192,7 @@ class HomePage extends Component {
             <MUIDataTable 
               title={"Subscriptions"}
               data={this.state.subscriptions}
-              columns={this.state.columns}
+              columns={columns}
               options={options} />
           </ThemeProvider>
           <ModalPopup  
